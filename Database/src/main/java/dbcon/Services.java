aa8c9720c4eefa10cc71664a963ed221f5915dc0
YunboChow <96528637+YunboChow@users.noreply.github.com>
2022-03-30 10:55:10 +0200

    Added database module

---
+++ Database/src/main/java/dbcon/Services.java
@@ -0,0 +1,88 @@
+package dbcon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Services {
+    public Connection getDatabaseConnection(){
+        String url = "jdbc:postgresql://pgserver.mau.se:5432/am2578";
+        String user = "am2578";
+        String password = "29zptibk";
+        Connection con = null;
+
+        try{
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection Established");
+            return con;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void insertNewUser(String email, String name, String password) throws SQLException {
+        Connection con = this.getDatabaseConnection();
+        PreparedStatement pstmt = con.prepareStatement("call training.insertNewUser(?,?,?)");
+        pstmt.setString(1, email);
+        pstmt.setString(2, name);
+        pstmt.setString(3, password);
+
+        pstmt.execute();
+        pstmt.close();
+        con.close();
+    }
+
+    public void insertNewExercise(String name, String description, String primaryMuscleGroup, String secondaryMuscleGroup) throws SQLException{
+        Connection con = this.getDatabaseConnection();
+        PreparedStatement pstmt = con.prepareStatement("call training.insertNewExercise(?,?,?,?)");
+        pstmt.setString(1, name);
+        pstmt.setString(2, description);
+        pstmt.setString(3, primaryMuscleGroup);
+        pstmt.setString(4, secondaryMuscleGroup);
+
+        pstmt.execute();
+        pstmt.close();
+        con.close();
+
+    }
+
+    //TODO: lägg till en privat metod som checkar om exerciseId samt workoutId redan finns. genom select XX where YY + if() sats
+    public void insertExerciseInToWorkout(int exerciseId, int workoutId) throws SQLException{
+        Connection con = this.getDatabaseConnection();
+        PreparedStatement pstmt = con.prepareStatement("call training.insertExerciseInToWorkout(?,?)");
+        pstmt.setInt(1, exerciseId);
+        pstmt.setInt(2, workoutId);
+
+        pstmt.execute();
+        pstmt.close();
+        con.close();
+    }
+
+    //creatorEmail == email som man är inloggad på
+    public void insertNewWorkout(String name, String creatorEmail, String description, String tag1, String tag2, String tag3) throws SQLException{
+        Connection con = this.getDatabaseConnection();
+        PreparedStatement pstmt = con.prepareStatement("Call training.insertNewWorkout(?,?,?,?,?,?)");
+        pstmt.setString(1, name);
+        pstmt.setString(2, creatorEmail);
+        pstmt.setString(3, description);
+        pstmt.setString(4, tag1);
+        pstmt.setString(5, tag2);
+        pstmt.setString(6, tag3);
+
+        pstmt.execute();
+        pstmt.close();
+        con.close();
+    }
+
+    
+
+
+
+
+
+
+
+
+}
